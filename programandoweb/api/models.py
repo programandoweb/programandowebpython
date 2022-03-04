852c0546735baac47d9008a00405698664223ccd
JORGE MENDEZ <lic.jorgemendez@gmail.com>
2022-03-04 07:42:32 -0500

    Se hizo migratrions con la estructura que creé de usuarios pgrw_users

--- programandoweb/api/models.py
+++ programandoweb/api/models.py
@@ -1,3 +1,9 @@
+from csv import unregister_dialect
 from django.db import models
+from django.forms import PasswordInput
+class Pgrw_User(models.Model):
+    names       =   models.CharField(max_length=30)
+    lastnames   =   models.CharField(max_length=30)
+    email       =   models.CharField(max_length=150)
+    password    =   models.CharField(max_length=250)  
 
-# Create your models here.
